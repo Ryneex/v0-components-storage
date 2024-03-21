fca15e24e3b9c75bfb6a6b7c4816a02eb8614b31
MI Sajid <misajidsa@gmail.com>
2024-03-22 01:02:24 +0600

    feat: hide form on production

--- src/app/(root)/Card.tsx
+++ src/app/(root)/Card.tsx
@@ -15,17 +15,19 @@ export default function Card({ data }) {
     function handleCopy() {
         navigator.clipboard.writeText(data.code);
         toast.success("Code copied to clipboard", {
-            className: "!bg-slate-800 !text-white"
-        })
+            className: "!bg-slate-800 !text-white",
+        });
     }
     return (
         <div className="p-5 border relative rounded-md border-slate-300">
             <div onClick={handleCopy} className="absolute top-2 left-2 z-10 rounded-full h-7 aspect-square select-none flex items-center justify-center cursor-pointer bg-slate-800 text-white">
                 <FaRegCopy />
             </div>
-            <div onClick={handleRemove} className="absolute top-2 right-2 z-10 rounded-full h-7 aspect-square select-none flex items-center justify-center cursor-pointer bg-red-400 text-white">
-                <LuTrash />
-            </div>
+            {process.env.NODE_ENV === "development" && (
+                <div onClick={handleRemove} className="absolute top-2 right-2 z-10 rounded-full h-7 aspect-square select-none flex items-center justify-center cursor-pointer bg-red-400 text-white">
+                    <LuTrash />
+                </div>
+            )}
             <div className="w-[410px] h-[230px] relative overflow-hidden">
                 <iframe className="absolute pointer-events-none origin-top-left scale-[0.4] aspect-video" src={`/components/${data.id}`} title="Generated UI from the prompt" style={{ width: 1024 }}></iframe>
             </div>
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -19,7 +19,9 @@ export default function page() {
     return (
         <div>
             <Toaster />
-            <AddNewForm />
+            {process.env.NODE_ENV === "development" && <AddNewForm />}
+            <h1 className="p-5 text-xl font-bold text-black/70">Available Components</h1>
+            {data.length === 0 && <div className="px-5 text-black/50 font-medium">You haven't added any components</div>}
             <div className="flex flex-wrap px-5 gap-5">
                 {data.map((data: any, i: any) => (
                     <Card key={i} data={data} />
